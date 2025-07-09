500033c61af0ee8eaf9f573c68f3969a88d5fa7f
Taylor O'Reilly <taylor@taylororeilly.dev>
2025-07-09 02:15:01 -0400

    fix formatting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,45 +6,45 @@ import { OrganizationCard } from "./organization-card";
 import AccountSwitcher from "@/components/account-switch";
 
 export default async function DashboardPage() {
-	const [session, activeSessions, deviceSessions, organization, subscriptions] =
-		await Promise.all([
-			auth.api.getSession({
-				headers: await headers(),
-			}),
-			auth.api.listSessions({
-				headers: await headers(),
-			}),
-			auth.api.listDeviceSessions({
-				headers: await headers(),
-			}),
-			auth.api.getFullOrganization({
-				headers: await headers(),
-			}),
-			auth.api.listActiveSubscriptions({
-				headers: await headers(),
-			}),
-		]).catch((e) => {
-			console.log(e);
-			throw redirect("/sign-in");
-		});
-	return (
-		<div className="w-full">
-			<div className="flex gap-4 flex-col">
-				<AccountSwitcher
-					sessions={JSON.parse(JSON.stringify(deviceSessions))}
-				/>
-				<UserCard
-					session={JSON.parse(JSON.stringify(session))}
-					activeSessions={JSON.parse(JSON.stringify(activeSessions))}
-					subscription={subscriptions.find(
-						(sub) => sub.status === "active" || sub.status === "trialing",
-					)}
-				/>
-				<OrganizationCard
-					session={JSON.parse(JSON.stringify(session))}
-					activeOrganization={JSON.parse(JSON.stringify(organization))}
-				/>
-			</div>
-		</div>
-	);
+  const [session, activeSessions, deviceSessions, organization, subscriptions] =
+    await Promise.all([
+      auth.api.getSession({
+        headers: await headers(),
+      }),
+      auth.api.listSessions({
+        headers: await headers(),
+      }),
+      auth.api.listDeviceSessions({
+        headers: await headers(),
+      }),
+      auth.api.getFullOrganization({
+        headers: await headers(),
+      }),
+      auth.api.listActiveSubscriptions({
+        headers: await headers(),
+      }),
+    ]).catch((e) => {
+      console.log(e);
+      throw redirect("/sign-in");
+    });
+  return (
+    <div className="w-full">
+      <div className="flex flex-col gap-4">
+        <AccountSwitcher
+          sessions={JSON.parse(JSON.stringify(deviceSessions))}
+        />
+        <UserCard
+          session={JSON.parse(JSON.stringify(session))}
+          activeSessions={JSON.parse(JSON.stringify(activeSessions))}
+          subscription={subscriptions.find(
+            (sub) => sub.status === "active" || sub.status === "trialing",
+          )}
+        />
+        <OrganizationCard
+          session={JSON.parse(JSON.stringify(session))}
+          activeOrganization={JSON.parse(JSON.stringify(organization))}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -6,31 +6,31 @@ import { Logo } from "./logo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function Wrapper(props: { children: React.ReactNode }) {
-	return (
-		<div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex justify-center">
-			<div className="absolute pointer-events-none inset-0 md:flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] hidden"></div>
-			<div className="bg-white dark:bg-black border-b py-2 flex justify-between items-center border-border absolute z-50 w-full lg:w-8/12 px-4 md:px-1">
-				<Link href="/">
-					<div className="flex gap-2 cursor-pointer">
-						<Logo />
-						<p className="dark:text-white text-black">BETTER-AUTH.</p>
-					</div>
-				</Link>
-				<div className="z-50 flex items-center">
-					<ThemeToggle />
-				</div>
-			</div>
-			<div className="mt-20 lg:w-7/12 w-full">{props.children}</div>
-		</div>
-	);
+  return (
+    <div className="dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex min-h-screen w-full justify-center bg-white dark:bg-black">
+      <div className="pointer-events-none absolute inset-0 hidden items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] md:flex dark:bg-black"></div>
+      <div className="border-border absolute z-50 flex w-full items-center justify-between border-b bg-white px-4 py-2 md:px-1 lg:w-8/12 dark:bg-black">
+        <Link href="/">
+          <div className="flex cursor-pointer gap-2">
+            <Logo />
+            <p className="text-black dark:text-white">BETTER-AUTH.</p>
+          </div>
+        </Link>
+        <div className="z-50 flex items-center">
+          <ThemeToggle />
+        </div>
+      </div>
+      <div className="mt-20 w-full lg:w-7/12">{props.children}</div>
+    </div>
+  );
 }
 
 const queryClient = new QueryClient();
 
 export function WrapperWithQuery(props: { children: React.ReactNode | any }) {
-	return (
-		<QueryClientProvider client={queryClient}>
-			{props.children}
-		</QueryClientProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
 }
